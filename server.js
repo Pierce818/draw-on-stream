4134c083d1a2630ff88ae6b686d32e7fdb2b18d8
Anthony Pierce <Anthonypierce043@gmail.com>
2026-04-08 18:21:56 -0500

    Update: modern UI, persistent Draw Here, vote-to-clear, squares, overlay usernames

--- server.js
+++ server.js
@@ -6,22 +6,34 @@ const io = require("socket.io")(http);
 app.use(express.static(__dirname));
 
 let connectedUsers = [];
+let votes = {};
+const zoneCount = 10;
 
 io.on("connection", socket=>{
   console.log("User connected");
 
-  // Assign zone
-  const index = connectedUsers.length % 10; // max 10 zones
-  const width = 1 / 10;
+  const index = connectedUsers.length % zoneCount;
   connectedUsers.push(socket.id);
-  socket.emit("zoneAssigned",{index,width});
+  socket.emit("zoneAssigned",{index,width:1/zoneCount});
 
-  socket.on("draw", data=>{
-    io.emit("draw", data); // send to overlay
+  // Broadcast drawing
+  socket.on("draw", data=>io.emit("draw", data));
+
+  // Vote to clear
+  socket.on("voteClear", user=>{
+    votes[socket.id] = true;
+    const voteCount = Object.keys(votes).length;
+    const majority = Math.ceil(connectedUsers.length/2);
+
+    if(voteCount >= majority){
+      io.emit("clearCanvas");
+      votes = {}; // reset
+    }
   });
 
   socket.on("disconnect", ()=>{
     connectedUsers = connectedUsers.filter(id => id!==socket.id);
+    delete votes[socket.id];
   });
 });
 
